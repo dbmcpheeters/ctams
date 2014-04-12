611b255348aab493136f718026546593118f3db7
Andrew Trimble <andy.trimble@gmail.com>
2014-04-12 14:52:25 -0600

    Added a placehoder for venues.

---
+++ ui/src/main/java/org/wuspba/ctams/ui/client/VenueExplorerPanel.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.wuspba.ctams.ui.client;
+
+import com.smartgwt.client.widgets.Canvas;
+import com.smartgwt.client.widgets.Label;
+import com.smartgwt.client.widgets.layout.VLayout;
+
+/**
+ *
+ * @author atrimble
+ */
+public class VenueExplorerPanel extends VLayout {
+    
+    public static class Factory implements PanelFactory {
+        private String id;
+
+        @Override
+        public Canvas create() {
+            VenueExplorerPanel panel = new VenueExplorerPanel();
+            id = panel.getID();
+            return panel;
+        }
+
+        @Override
+        public String getID() {
+            return id;
+        }
+    }
+
+    public VenueExplorerPanel() {
+        Label title = new Label("Coming soon!");
+        title.setStyleName("sgwtTitle");
+        title.setWidth(500);
+        addMember(title);
+    }
+}
